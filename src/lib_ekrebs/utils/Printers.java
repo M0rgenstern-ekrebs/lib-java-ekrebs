5148e8ea0d20618d426ee6277525f952bb5adac8
ekrebs <ekrebs.contact+Git@gmail.com>
2026-02-03 14:11:58 +0100

    print imbricated objects now made easier by AddTabLevel()
    + AddTablevel(str, int)
        modified:   src/lib_ekrebs/utils/Printers.java

--- src/lib_ekrebs/utils/Printers.java
+++ src/lib_ekrebs/utils/Printers.java
@@ -1,5 +1,9 @@
 package lib_ekrebs.utils;
 
-public class Printers {
-    
+public class Printers
+{
+    public static String AddTabLevel(String str, int TabLevel)
+    {
+        return (str.replace("\n", "\n"+"\t".repeat(TabLevel)));
+    }
 }
