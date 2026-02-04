f0b62ed81db8068b9bbb925a911f3b5ecf1338e4
ekrebs <ekrebs.contact+Git@gmail.com>
2026-02-04 14:39:00 +0100

    + toMacaron(str, COLOR)         to be used to display object name or titles and such ! + toDisplay(str)         to be used at the end of each object toString function !
    	modified:   src/lib_ekrebs/defines/box_drawing_chars/BoxDrawingChars.java
    	modified:   src/lib_ekrebs/utils/Printers.java

--- src/lib_ekrebs/defines/box_drawing_chars/BoxDrawingChars.java
+++ src/lib_ekrebs/defines/box_drawing_chars/BoxDrawingChars.java
@@ -10,38 +10,38 @@ package lib_ekrebs.defines.box_drawing_chars;
 public final class BoxDrawingChars
 {
     // ============ HEAVY LINES ============
-    public static final String CROSS_HEAVY       = "╋";  // U+257B
-    public static final String HORIZ_HEAVY       = "━";  // U+2501
-    public static final String VERT_HEAVY        = "┃";  // U+2573
-    public static final String TOP_LEFT_HEAVY    = "┏";  // U+250F
-    public static final String TOP_RIGHT_HEAVY   = "┓";  // U+2513
-    public static final String BOT_LEFT_HEAVY    = "┗";  // U+2517
-    public static final String BOT_RIGHT_HEAVY   = "┛";  // U+251B
+    public static final String LINE_HEAVY_CROSS       = "╋";  // U+257B
+    public static final String LINE_HEAVY_HORIZ       = "━";  // U+2501
+    public static final String LINE_HEAVY_VERTICAL    = "┃";  // U+2573
+    public static final String LINE_HEAVY_TOP_LEFT    = "┏";  // U+250F
+    public static final String LINE_HEAVY_TOP_RIGHT   = "┓";  // U+2513
+    public static final String LINE_HEAVY_BOT_LEFT    = "┗";  // U+2517
+    public static final String LINE_HEAVY_BOT_RIGHT   = "┛";  // U+251B
 
     // ============ LIGHT LINES ============
-    public static final String CROSS_LIGHT      = "┼";  // U+253C
-    public static final String HORIZ_LIGHT      = "─";  // U+2500
-    public static final String VERT_LIGHT       = "│";  // U+2502
-    public static final String TEE_UP_LIGHT     = "┬";  // U+252C
-    public static final String TEE_RIGHT_LIGHT  = "┤";  // U+2524
-    public static final String TEE_DOWN_LIGHT   = "┴";  // U+2534
-    public static final String TEE_LEFT_LIGHT   = "├";  // U+251C
-    public static final String TOP_LEFT_LIGHT   = "┌";  // U+250C
-    public static final String TOP_RIGHT_LIGHT  = "┐";  // U+2510
-    public static final String BOT_LEFT_LIGHT   = "└";  // U+2514
-    public static final String BOT_RIGHT_LIGHT  = "┘";  // U+2518
+    public static final String LINE_LIGHT_CROSS      = "┼";  // U+253C
+    public static final String LINE_LIGHT_HORIZ      = "─";  // U+2500
+    public static final String LINE_LIGHT_VERTICAL   = "│";  // U+2502
+    public static final String LINE_LIGHT_TEE_UP     = "┬";  // U+252C
+    public static final String LINE_LIGHT_TEE_RIGHT  = "┤";  // U+2524
+    public static final String LINE_LIGHT_TEE_DOWN   = "┴";  // U+2534
+    public static final String LINE_LIGHT_TEE_LEFT   = "├";  // U+251C
+    public static final String LINE_LIGHT_TOP_LEFT   = "┌";  // U+250C
+    public static final String LINE_LIGHT_TOP_RIGHT  = "┐";  // U+2510
+    public static final String LINE_LIGHT_BOT_LEFT   = "└";  // U+2514
+    public static final String LINE_LIGHT_BOT_RIGHT  = "┘";  // U+2518
 
     // ============ DOUBLE LINES ============
-    public static final String CROSS_DOUBLE     = "╬";  // U+256C
-    public static final String HORIZ_DOUBLE     = "═";  // U+2550
-    public static final String VERT_DOUBLE      = "║";  // U+2551
-    public static final String TEE_UP_DOUBLE    = "╦";  // U+2556
-    public static final String TEE_RIGHT_DOUBLE = "╣";  // U+2563
-    public static final String TEE_DOWN_DOUBLE  = "╩";  // U+2559
-    public static final String TEE_LEFT_DOUBLE  = "╠";  // U+2560
-    public static final String TOP_LEFT_DBL     = "╔";  // U+2552
-    public static final String TOP_RIGHT_DBL    = "╗";  // U+2553
-    public static final String BOT_LEFT_DBL     = "╚";  // U+255A
-    public static final String BOT_RIGHT_DBL    = "╝";  // U+255D
+    public static final String LINE_DOUBLE_CROSS        = "╬";  // U+256C
+    public static final String LINE_DOUBLE_HORIZ        = "═";  // U+2550
+    public static final String LINE_DOUBLE_VERTICAL     = "║";  // U+2551
+    public static final String LINE_DOUBLE_TEE_UP       = "╦";  // U+2556
+    public static final String LINE_DOUBLE_TEE_RIGHT    = "╣";  // U+2563
+    public static final String LINE_DOUBLE_TEE_DOWN     = "╩";  // U+2559
+    public static final String LINE_DOUBLE_TEE_LEFT     = "╠";  // U+2560
+    public static final String LINE_DOUBLE_TOP_LEFT     = "╔";  // U+2552
+    public static final String LINE_DOUBLE_TOP_RIGHT    = "╗";  // U+2553
+    public static final String LINE_DOUBLE_BOT_LEFT     = "╚";  // U+255A
+    public static final String LINE_DOUBLE_BOT_RIGHT    = "╝";  // U+255D
 }
 
--- src/lib_ekrebs/utils/Printers.java
+++ src/lib_ekrebs/utils/Printers.java
@@ -1,9 +1,36 @@
 package lib_ekrebs.utils;
+import static lib_ekrebs.defines.ECMA_48.ECMA_SGR_codes.*;
+import static lib_ekrebs.defines.box_drawing_chars.BoxDrawingChars.*;
 
 public class Printers
 {
     public static String AddTabLevel(String str, int TabLevel)
     {
-        return (str.replace("\n", "\n"+"\t".repeat(TabLevel)));
+        return (str.replace("\n", "\n"+LINE_LIGHT_VERTICAL+"\t".repeat(TabLevel)));
+    }
+
+    public static String toDisplay(String str)
+    {
+        if (str == null)
+            System.out.println(BG_RED+" Error:"+RESET+"str = null");
+        return(str.replace("\n", "\n│").replace("│─", "┌─").replace("│\n┌", "│\n├")+"\n└");
+    }
+
+    public static String toMacaron(String str, String color)
+    {
+        String result;
+
+        if (str == null)
+            return "";
+        result = str.trim();
+        if (!result.startsWith(" "))
+        {
+            result = " "+result;
+        }
+        if (!result.endsWith(":"))
+        {
+            result = result + ":";
+        }
+        return ("\n─"+UNDERLINE+color+result+RESET);
     }
 }
