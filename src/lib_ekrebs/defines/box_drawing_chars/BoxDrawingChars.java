9ebb912a1f21043dfde7aec2d69af0dbf873e842
ekrebs <ekrebs.contact+Git@gmail.com>
2026-02-04 10:56:42 +0100

    + now with Box drawing characters (HEAVY, LIGHT, and DOUBLE)         new file:   src/lib_ekrebs/defines/box_drawing_chars/BoxDrawingChars.java

---
+++ src/lib_ekrebs/defines/box_drawing_chars/BoxDrawingChars.java
@@ -0,0 +1,47 @@
+package lib_ekrebs.defines.box_drawing_chars;
+
+//
+
+/**
+ * Box drawing characters - partial set
+ * @see <a href="https://en.wikipedia.org/wiki/Box-drawing_characters">Wikipedia Box drawing characters</a>
+ *
+ */
+public final class BoxDrawingChars
+{
+    // ============ HEAVY LINES ============
+    public static final String CROSS_HEAVY       = "╋";  // U+257B
+    public static final String HORIZ_HEAVY       = "━";  // U+2501
+    public static final String VERT_HEAVY        = "┃";  // U+2573
+    public static final String TOP_LEFT_HEAVY    = "┏";  // U+250F
+    public static final String TOP_RIGHT_HEAVY   = "┓";  // U+2513
+    public static final String BOT_LEFT_HEAVY    = "┗";  // U+2517
+    public static final String BOT_RIGHT_HEAVY   = "┛";  // U+251B
+
+    // ============ LIGHT LINES ============
+    public static final String CROSS_LIGHT      = "┼";  // U+253C
+    public static final String HORIZ_LIGHT      = "─";  // U+2500
+    public static final String VERT_LIGHT       = "│";  // U+2502
+    public static final String TEE_UP_LIGHT     = "┬";  // U+252C
+    public static final String TEE_RIGHT_LIGHT  = "┤";  // U+2524
+    public static final String TEE_DOWN_LIGHT   = "┴";  // U+2534
+    public static final String TEE_LEFT_LIGHT   = "├";  // U+251C
+    public static final String TOP_LEFT_LIGHT   = "┌";  // U+250C
+    public static final String TOP_RIGHT_LIGHT  = "┐";  // U+2510
+    public static final String BOT_LEFT_LIGHT   = "└";  // U+2514
+    public static final String BOT_RIGHT_LIGHT  = "┘";  // U+2518
+
+    // ============ DOUBLE LINES ============
+    public static final String CROSS_DOUBLE     = "╬";  // U+256C
+    public static final String HORIZ_DOUBLE     = "═";  // U+2550
+    public static final String VERT_DOUBLE      = "║";  // U+2551
+    public static final String TEE_UP_DOUBLE    = "╦";  // U+2556
+    public static final String TEE_RIGHT_DOUBLE = "╣";  // U+2563
+    public static final String TEE_DOWN_DOUBLE  = "╩";  // U+2559
+    public static final String TEE_LEFT_DOUBLE  = "╠";  // U+2560
+    public static final String TOP_LEFT_DBL     = "╔";  // U+2552
+    public static final String TOP_RIGHT_DBL    = "╗";  // U+2553
+    public static final String BOT_LEFT_DBL     = "╚";  // U+255A
+    public static final String BOT_RIGHT_DBL    = "╝";  // U+255D
+}
+
